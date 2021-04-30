b67a7ca6c75ae557786b1bbe1ab4167cebf4ff56
Harold <hlgoco.hg@gmail.com>
2021-04-30 20:45:59 +0800

    no message

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,7 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {999, 888, 2, -991});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
