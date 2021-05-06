bc745f9a8d2fd86cbc116807fe394d31f959df36
Harold <hlgoco.hg@gmail.com>
2021-05-06 22:07:51 +0800

    Chapter 2 activity

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,42 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 public class GHappyTest {
+
+    @Test
+    public void test1() {
+        GHappy happy = new GHappy();
+        boolean isHappy = happy.gHappy("xxggxx");
+        Assertions.assertTrue(isHappy);
+    }
+
+    @Test
+    public void test2() {
+        GHappy happy = new GHappy();
+        boolean isHappy = happy.gHappy("xxgxx");
+        Assertions.assertFalse(isHappy);
+    }
+
+    @Test
+    public void test3() {
+        GHappy happy = new GHappy();
+        boolean isHappy = happy.gHappy("xxggyygxx");
+        Assertions.assertFalse(isHappy);
+    }
+
+    @Test
+    public void test4() {
+        GHappy happy = new GHappy();
+        boolean isHappy = happy.gHappy("ggxxggyyggxx");
+        Assertions.assertFalse(isHappy);
+    }
+
+    @Test
+    public void test5() {
+        GHappy happy = new GHappy();
+        boolean isHappy = happy.gHappy("ggxxggyyggxxgg");
+        Assertions.assertFalse(isHappy);
+    }
+
 }
